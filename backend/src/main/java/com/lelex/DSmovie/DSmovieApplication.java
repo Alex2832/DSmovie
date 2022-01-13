9057e29d5946008c6999a67bc20dabea34f43eb2
Alex2832 <lex-dewes3268@hotmail.com>
2022-01-13 18:14:57 -0300

    project created

---
+++ backend/src/main/java/com/lelex/DSmovie/DSmovieApplication.java
@@ -0,0 +1,13 @@
+package com.lelex.DSmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSmovieApplication.class, args);
+	}
+
+}
